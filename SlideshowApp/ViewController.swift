9f6a64166430871a466947cf76ddb3de4b5ebaba
ToshiyaYoneda <toshiyayonedadesu@gmail.com>
2019-09-18 06:55:48 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by ToshiyaYoneda on 2019/09/18.
+//  Copyright © 2019 ToshiyaYoneda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
